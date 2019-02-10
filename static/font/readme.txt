93fbee2cb3d293d5b4a1b607f5e7815cdd9611f1
songyifan427 <songyifan427@163.com>
2019-02-10 23:51:43 +0800

    syfadd2.10

---
+++ static/font/readme.txt
@@ -0,0 +1,12 @@
+fontSupport
+定位
+.icon-location
+ 
+编写
+.icon-xiazai43
+ 
+删除
+.icon-del
+ 
+手机
+.icon-icon-test
--- static/js/shoppingCart.js
+++ static/js/shoppingCart.js
@@ -1,5 +1,20 @@
 // function vmmv(Area,monitorNode,attributeName,responseFunction){
-    
+    // var temp;
+    // var obj=document.querySelector("#input");
+    // obj.oninput=function(){
+    //     obj.name=obj.value;
+    // }
+    // Object.defineProperty(obj,"name",{configurable:true,enumerable:true,
+    // set:function(val){
+    //     if(temp!=val){
+    //         console.log("值变化了")
+    //     }
+    //     temp=val
+    // },
+    // get:function(val){
+    //     return temp
+    // },
+    // })  
 // }
 // sub price
 // total price

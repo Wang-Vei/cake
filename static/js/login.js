d08746ef19593b17f2e41d9e57dd57c81c03a82c
songyifan427 <songyifan427@163.com>
2019-02-11 23:26:01 +0800

    syfadd2.11

---
+++ static/js/login.js
@@ -0,0 +1,8 @@
+function goRegister(){
+    document.querySelector(".login").style.display="none";
+    document.querySelector(".register").style.display="block";
+}
+function goLogin(){
+    document.querySelector(".login").style.display="block";
+    document.querySelector(".register").style.display="none";
+}
